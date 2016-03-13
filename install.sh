942402375ac09903e9f16812ebddcc40bf3fc8f4
ko.mizoguchi <r.koshi.0514@gmail.com>
2016-03-13 12:27:35 +0900

    change method

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
-cp -r .zsh $HOME
-cp .tmux.conf $HOME
-cp .zshrc $HOME
-cp .vimrc $HOME
-cp -r .vim/conf.d $HOME/.vim
-cp -r .vim/colors $HOME/.vim
+
+for file in `find files -maxdepth 1 -name ".*"`; do
+    ln -s shellenv/$file $HOME
+done
+
+#ln -s shellenv/.tmux.conf $HOME
+#ln -s shellenv/.zsh $HOME
+# cp -r .zsh $HOME
+# cp .zshrc $HOME
+# cp .vimrc $HOME
+# cp -r .vim/conf.d $HOME/.vim
+# cp -r .vim/colors $HOME/.vim
