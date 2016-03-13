25fe5925f3b8cbaa6bb435453f687ef0d4fb7df4
ko.mizoguchi <r.koshi.0514@gmail.com>
2016-03-13 12:33:14 +0900

    remove comment

--- install.sh
+++ install.sh
@@ -2,11 +2,3 @@
 for file in `find files -maxdepth 1 -name ".*"`; do
     ln -s shellenv/$file $HOME
 done
-
-#ln -s shellenv/.tmux.conf $HOME
-#ln -s shellenv/.zsh $HOME
-# cp -r .zsh $HOME
-# cp .zshrc $HOME
-# cp .vimrc $HOME
-# cp -r .vim/conf.d $HOME/.vim
-# cp -r .vim/colors $HOME/.vim
